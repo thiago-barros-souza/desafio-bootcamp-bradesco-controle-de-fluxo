dedd13c47657ffa2e6fc59c3f9163524a4608adb
Thiago Barros <thiago.barros@perinity.com>
2025-03-17 23:33:24 -0300

    Fechando recurso scanner

--- src/main/java/org/desafio/Contador.java
+++ src/main/java/org/desafio/Contador.java
@@ -19,6 +19,8 @@ public class Contador {
 		} catch (ParametrosInvalidosException e) {
 			System.out.println(e.getMessage());
 		}
+
+		input.close();
 	}
 
 	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
